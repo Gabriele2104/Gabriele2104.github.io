1ce558f841096a476737f82f0d158cf6287f3e78
Riccardi Enrico Gabriele <riccardigabriele4@gmail.com>
2025-08-15 19:25:37 +0200

    Prima versione

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// La somma che viene inserita nell'input, deve essere divisa e allocata nelle voci presenti sotto
+
+const input = document.getElementById("input");
+const button = document.getElementById("btn");
+let spesa = document.getElementById("Spesa");
+let bollette = document.getElementById("Bollette");
+let affitto = document.getElementById("Affitto");
+let macchina = document.getElementById("Macchina");
+let risparmio = document.getElementById("Risparmio");
+let varie = document.getElementById("Varie");
+
+function gestioneSoldi(sommaSoldi){
+        spesa.value = sommaSoldi * .20;
+        bollette.value = sommaSoldi * .15;
+        affitto.value = sommaSoldi * .30;
+        macchina.value = sommaSoldi * .10;
+        risparmio.value = sommaSoldi * .10;
+        varie.value = sommaSoldi * .10;
+}
+
+input.addEventListener("keyup", function (){
+    gestioneSoldi(input.value);
+})
+
+button.addEventListener("click", function (gestioneSoldi){
+    spesa.textContent = spesa.value;
+    bollette.textContent = bollette.value ;
+    affitto.textContent = affitto.value ;
+    macchina.textContent = macchina.value ;
+    risparmio.textContent = risparmio.value ;
+    varie.textContent = varie.value ;
+})
